9f74e538997c7114873ead0b99c5e651335caf64
wdxtub <dacrocodilee@gmail.com>
2015-11-29 22:52:46 -0500

    kmeans 77%

--- code/testGist.m
+++ code/testGist.m
@@ -8,30 +8,23 @@ fprintf('Start Extracting feature...patient\n');
 tic;
 
 % number of pictures
-N = 500;
+N = 50;
 
 for t=1:N % test with 500 first
     fprintf('Extract gist feature for Image %d / %d...\n',t, N);
-    red   = trainX(t,1:1024);
-    green = trainX(t,1025:2048);
-    blue  = trainX(t,2049:3072);
-    
-    image(:,:,1) = reshape(red  ,32,32);
-    image(:,:,2) = reshape(green,32,32);
-    image(:,:,3) = reshape(blue ,32,32);
     
     clear param;
     param.orientationsPerScale = [8 8 8 8];
     param.numberBlocks = 4;
     param.fc_prefilt = 4;
 
-    [gist1, param] = LMgist(image, '', param);
+    [gist1, param] = LMgist(imgs(:,:,:,t), '', param);
     fprintf(fid,'%d ',trainY(t));
     for ii=1:512 
         %fprintf(fid,'%d:%g %d:%g %d:%g %d:%g %d:%g %d:%g %d:%g %d:%g ',ii,gist1(1,ii),ii+2,gist1(1,ii+2),ii+3,gist1(1,ii+3),ii+4,gist1(1,ii+4),ii+5,gist1(1,ii+5),ii+6,gist1(1,ii+6),ii+7,gist1(1,ii+7));
         fprintf(fid,'%d:%g ', ii, gist1(1, ii));
     end
-    if (mod(t,100)==0)/
+    if (mod(t,100)==0)
         t;
     end
     fprintf(fid,'\n');
